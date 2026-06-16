92c15e921896d90cb4b2af078153424e39e03a36
Prabhav Kondapally <kondapallyprabhav@gmail.com>
2026-06-16 18:41:54 -0500

    Added functionality to the chart review page

--- pages/Chart_Review.py
+++ pages/Chart_Review.py
@@ -3,8 +3,72 @@ import streamlit as st
 st.title("Clinical Chart Review")
 st.write("---")
 
-st.info("Medical chart AI summaries and electronic health record (EHR) validation toolsets will load here.")
+st.subheader("Hospital Overview")
 
+hp_count = len(st.session_state.get("high_priority_room", {}))
+normal_count = len(st.session_state.get("normal_room", {}))
+waitlist_count = len(st.session_state.get("waitlist", {}))
+
+total_patients = hp_count + normal_count + waitlist_count
+
+all_dosages = []
+
+for patient in st.session_state.get("high_priority_room", {}).values():
+    all_dosages.append(patient[1])
+
+for patient in st.session_state.get("normal_room", {}).values():
+    all_dosages.append(patient[1])
+
+for patient in st.session_state.get("waitlist", {}).values():
+    all_dosages.append(patient[1])
+
+if all_dosages:
+    avg_dosage = round(sum(all_dosages) / len(all_dosages), 2)
+    highest_dosage = max(all_dosages)
+    lowest_dosage = min(all_dosages)
+else:
+    avg_dosage = 0
+    highest_dosage = 0
+    lowest_dosage = 0
+
+bed_limit = st.session_state.get("bed_limit", 2)
+
+if bed_limit > 0:
+    bed_utilization = round((hp_count / bed_limit) * 100, 1)
+else:
+    bed_utilization = 0
+
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.metric("Total Patients", total_patients)
+
+with col2:
+    st.metric("High Priority", hp_count)
+
+with col3:
+    st.metric("Normal Room", normal_count)
+
+with col4:
+    st.metric("Waitlist", waitlist_count)
+
+st.write("")
+
+col5, col6, col7, col8 = st.columns(4)
+
+with col5:
+    st.metric("Average Dosage (mg)", avg_dosage)
+
+with col6:
+    st.metric("Highest Dosage (mg)", highest_dosage)
+
+with col7:
+    st.metric("Lowest Dosage (mg)", lowest_dosage)
+
+with col8:
+    st.metric("Bed Utilization (%)", bed_utilization)
+
+st.write("---")
 # Branding footer
 st.markdown("<br><br><br><br><br><br>", unsafe_allow_html=True)
 st.divider()
